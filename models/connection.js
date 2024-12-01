0718feb0970a826614cc3a1d5509947ed5b3f6b9
Thomas <thomas86.cornu@gmail.com>
2024-12-01 18:49:57 +0100

    add my backend

---
+++ models/connection.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+mongoose.set('strictQuery', false);
+const connectionString = process.env.CONNECTION_STRING;
+
+mongoose.connect(connectionString, { connectTimeoutMS: 2000 })
+  .then(() => console.log('Database connected'))
+  .catch(error => console.error(error));
